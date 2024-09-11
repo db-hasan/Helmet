72018da75416a0d7321df4a7fc4c1dcb0ce0ab25
dbhasan <infoalihasanbd@gmail.com>
2024-09-11 12:33:42 +0600

    role and permission

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    private $permissions = [
+        'user-index', 'user-create', 'user-edit',
+        'role-index', 'role-create', 'role-edit',
+        'dashboard-index',
+        'profle-update',
+    ];
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        foreach ($this->permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        };
+
+        $user = User::create([
+            'name' => 'Developer',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('12345678'),
+            'status' => "1",
+        ]);
+
+        $role = Role::create(['name' => 'superadmin']);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->syncRoles([$role->id]);
+    }
+}
