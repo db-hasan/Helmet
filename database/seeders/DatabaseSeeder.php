8c6e0a2dcd54a10ffae2dffc13dbf41d2c3e2971
dbhasan <infoalihasanbd@gmail.com>
2024-09-12 12:03:13 +0600

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
+use App\Models\Type;
+use App\Models\Brand;
+use App\Models\Modeles;
+use App\Models\Size;
+use App\Models\Color;
+use App\Models\Certification;
+use App\Models\CostType;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +23,34 @@ class DatabaseSeeder extends Seeder
         'role-index', 'role-create', 'role-edit',
         'dashboard-index',
         'profle-update',
+    ];
+
+    private $types = [
+        'Full Face', 'Half Face', 'Modular', 'Half Shell',
+        'Open-Face', 'Cap', 'Dual Sport',
+    ];
+
+    private $brands = [
+        'Studds', 'Vega', 'LS2', 'AXOR', 'MT', 'Steelbird', 'SKT', 'IBK', 'Others',
+    ];
+
+    private $modeles = [
+        'THUNDER', 'BOLT', 'Others',
+    ];
+
+    private $sizes = [
+        'XS', 'S', 'M', 'L', 'XL', 'XXL',
+    ];
+    private $colors = [
+        'Blue', 'Red', 'Black', 'White', 'Pink', 'Pink', 'Grey', 'Yellow', 'Others',
+    ];
+
+    private $certifications = [
+        'BSTI', 'ISI', 'DOT', 'ECE',
+    ];
+
+    private $costtypes = [
+        'Labour', 'Storage', 'Rent', 'Utilities', 'Employee', 'Others',
     ];
 
     /**
@@ -27,6 +62,29 @@ class DatabaseSeeder extends Seeder
             Permission::create(['name' => $permission]);
         };
 
+        foreach ($this->types as $type) {
+            Type::create(['name' => $type]);
+        };
+        foreach ($this->brands as $brand) {
+            Brand::create(['name' => $brand]);
+        };
+        foreach ($this->modeles as $modele) {
+            Modeles::create(['name' => $modele]);
+        };
+        foreach ($this->sizes as $size) {
+            Size::create(['name' => $size]);
+        };
+        foreach ($this->colors as $color) {
+            Color::create(['name' => $color]);
+        };
+        foreach ($this->certifications as $certification) {
+            Certification::create(['name' => $certification]);
+        };
+
+        foreach ($this->costtypes as $costtype) {
+            CostType::create(['name' => $costtype]);
+        };
+
         $user = User::create([
             'name' => 'Super Admin',
             'email' => 'superadmin@gmail.com',
@@ -35,11 +93,9 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $role = Role::create(['name' => 'superadmin']);
-
         $permissions = Permission::pluck('id', 'id')->all();
 
         $role->syncPermissions($permissions);
-
         $user->syncRoles([$role->id]);
     }
 }
